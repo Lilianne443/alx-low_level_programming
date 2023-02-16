b851dba163882cf37fd1c8474a90486ceee572c7
Lilianne443 <lilynsuk@gmail.com>
2023-02-16 15:50:09 -0800

    “low_level_programming”

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
-*main - Entry point
-*Return: Always 0 (success)
-*/
-
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-      printf("with proper grammar, but the outcome is a piece of art,\n";
-      return (0);
- }
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
