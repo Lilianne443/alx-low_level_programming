ae1861dba3d51d65da04f1f3f7195801ace57268
Lilianne443 <lilynsuk@gmail.com>
2023-02-16 16:08:42 -0800

    “low_level_programming”

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
- *main -Entry poiny
- *Return: Always 0 (Success)
+ * main - A program that prints a line using printf functio
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
