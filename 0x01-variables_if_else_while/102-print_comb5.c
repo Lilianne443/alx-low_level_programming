c2c4290c270c7878115257e1718e78ede32b2150
Lilianne443 <lilynsuk@gmail.com>
2023-02-20 15:35:36 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 /**
- * main - writes a program that prints all possible different combination of two digits
- * Return: 0
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int c = 0;
-	int f_d;
-	int l_d;
 
-	int c2;
-	int f_d2;
-	int l_d2;
-	while (c <= 98)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c2 = 0;
-		while (c2 <= 99)
-		{
-			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
-			if (c < c2)
-			{
-				putchar(f_d);
-				putchar(l_d);
-				putchar(' ');
-				putchar(f_d2);
-				putchar(l_d2);
-				if (c != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+	for (q = p + 1; q <= 99; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
+
 	return (0);
 }
