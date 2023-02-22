0aea085326c782b1014844e58189836fbdb8e0a3
Lilianne443 <lilynsuk@gmail.com>
2023-02-21 16:42:33 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of jack bauer
- * Return: void
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return:void
  */
 
 void jack_bauer(void)
