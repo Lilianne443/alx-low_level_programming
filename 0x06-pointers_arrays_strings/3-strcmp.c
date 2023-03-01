675b8e4a96594f3d8cda81959cdefab2404a7ef4
Lilianne443 <lilynsuk@gmail.com>
2023-03-01 06:26:05 -0800

    pointers arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,23 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * Return: dest
+ * Return: s1[i] - s2[i]
  */
-char *_strncat(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (0);
 }
